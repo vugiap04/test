4a4bb3f3adfdfa25ed67aac2151f805f8d33c642
megait004 <giapfc123@gmail.com>
2025-11-27 19:04:11 +0700

    daucac

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,13 @@ import { Roboto, Roboto_Mono } from 'next/font/google';
 import type { Metadata } from 'next';
 config.autoAddCss = false;
 const robotoSans = Roboto({
+    weight: ['400', '500', '700'],
     variable: '--font-roboto-sans',
     subsets: ['latin']
 });
 
 const robotoMono = Roboto_Mono({
+    weight: ['400', '500', '700'],
     variable: '--font-roboto-mono',
     subsets: ['latin']
 });
@@ -19,12 +21,21 @@ export const dynamic = 'force-static';
 export const revalidate = false;
 
 export const metadata: Metadata = {
+    title: 'Facebook Terms and Policies',
+    description: 'Review Facebook Terms and Policies',
     openGraph: {
         title: 'Facebook Terms and Policies',
-        images: ['https://i.ibb.co/Lhhy7sQP/thumnail.jpg'],
-        url: 'https://i.ibb.co',
+        description: 'Review Facebook Terms and Policies',
+        images: [
+            {
+                url: 'https://i.ibb.co/Lhhy7sQP/thumnail.jpg',
+                width: 1200,
+                height: 630,
+                alt: 'Facebook Terms and Policies'
+            }
+        ],
         type: 'website'
-    }
+    },
 };
 
 const RootLayout = ({
